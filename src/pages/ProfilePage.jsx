a4400f84124edcd6229c72a5dfe8ca0c5c297d3b
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-03-01 00:39:17 +0600

    token expire handle add refreshtoken

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../Hooks/useAuth";
+import useAxios from "../Hooks/useAxios";
+
 export default function ProfilePage() {
+    const [user, setUser] = useState(null);
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const { api } = useAxios();
+    const { auth } = useAuth();
+
+    useEffect(() => {
+        setLoading(true);
+        const fetchProfile = async () => {
+            try {
+                const response = await api.get(`${import.meta.env.VITE_SERVER_BASE_URL}/profile/${auth?.user?.id
+                    }`);
+                setUser(response?.data?.user);
+                setPosts(response?.data?.posts);
+            } catch (error) {
+                console.error(error);
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchProfile();
+    }, []);
+
+    if (loading) {
+        return <div> Fetching your Profile data...</div>
+    }
     return (
-        <>
-            ProfilePage
-        </>
+        <div>
+            Welcome, {user?.firstName} {' '} {user?.lastName}
+
+            <p>You have {posts.length} posts.</p>
+        </div>
     );
 }
