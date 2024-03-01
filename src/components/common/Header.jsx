ab28fe479d4993d7c688df64049bee4c4123f45c
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2024-03-01 21:00:53 +0600

    home page

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -29,15 +29,17 @@ export default function Header() {
                     </button>
                     <Logout />
 
-                    <button className="flex-center !ml-8 gap-3">
-                        <span className="text-lg font-medium lg:text-xl">{user?.firstName} {user?.lastName}</span>
+                    <Link to="/me" className="flex-center !ml-8 gap-3">
+                        <span className="text-lg font-medium lg:text-xl">
+                            {user?.firstName} {user?.lastName}
+                        </span>
                         <img
                             className="max-h-[32px] max-w-[32px] lg:max-h-[44px] lg:max-w-[44px] rounded-full"
                             src={`${import.meta.env.VITE_SERVER_BASE_URL}/${user.avatar
                                 }`}
                             alt="avatar"
                         />
-                    </button>
+                    </Link>
                 </div>
             </div>
         </nav>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,50 @@
-import { Link } from "react-router-dom";
-import { useAuth } from "../Hooks/useAuth";
+import { useEffect, useReducer } from "react";
+import useAxios from "../Hooks/useAxios";
+import { actions } from "../actions";
+import PostList from "../components/posts/PostList";
+import { initialState, postReducer } from "../reducers/PostReducer";
 
 export default function HomePage() {
-    const { auth } = useAuth();
-    console.log(auth);
+    const [state, dispatch] = useReducer(postReducer, initialState);
+    const { api } = useAxios();
+
+    useEffect(() => {
+        dispatch({ type: actions.post.DATA_FETCHING });
+
+        const fetchPost = async () => {
+            try {
+                const response = await api.get(
+                    `${import.meta.env.VITE_SERVER_BASE_URL}/posts`
+                );
+                if (response.status === 200) {
+                    dispatch({
+                        type: actions.post.DATA_FETCHED,
+                        data: response.data,
+                    });
+                }
+            } catch (error) {
+                console.error(error);
+                dispatch({
+                    type: actions.post.DATA_FETCH_ERROR,
+                    error: error.message,
+                });
+            }
+        };
+
+        fetchPost();
+    }, []);
+
+    if (state?.loading) {
+        return <div> We are working...</div>;
+    }
+
+    if (state?.error) {
+        return <div> Error in fatching posts {state?.error?.message}</div>;
+    }
+
     return (
-        <>
-            HomePage
-            <Link to="/me">profile</Link>
-        </>
+        <div>
+            <PostList posts={state?.posts} />
+        </div>
     );
 }
